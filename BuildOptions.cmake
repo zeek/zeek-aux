7f5ac1a847da14a64a47b792a876de99683fda2d
Jon Siwek <jsiwek@ncsa.illinois.edu>
2010-10-20 13:16:02 -0500

    Added debug/release mode compiler flags and build summary output.

--- BuildOptions.cmake
+++ BuildOptions.cmake
@@ -8,6 +8,16 @@
 set(CMAKE_INSTALL_PREFIX /usr/local
     CACHE STRING "Installation directory" FORCE)
 
+set(ENABLE_DEBUG false
+    CACHE STRING "Compile with debugging symbols" FORCE)
+
+set(ENABLE_RELEASE false
+    CACHE STRING "Use -O3 compiler optimizations" FORCE)
+
+##
+## Configure Dependencies for Non-Standard Paths
+##
+
 # Uncomment to specify a custom prefix that contains the libpcap installation.
 #set(PCAP_ROOT_DIR path/to/your/pcap)
 
