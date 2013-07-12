accd9c0b5f206a886ce29b85712c49b174a81816
Robin Sommer <robin@icir.org>
2013-07-11 18:02:41 -0700

    A script to setup a skeleton for a new dynamic plugin.

---
+++ plugin-support/skeleton/src/Plugin.cc
@@ -0,0 +1,9 @@
+
+#include <plugin/Plugin.h>
+
+BRO_PLUGIN_BEGIN(@PLUGIN_NAMESPACE@, @PLUGIN_NAME@)
+	BRO_PLUGIN_VERSION(1);
+	BRO_PLUGIN_DESCRIPTION("<Insert brief description of plugin>");
+	BRO_PLUGIN_BIF_FILE(events);
+	BRO_PLUGIN_BIF_FILE(functions);
+BRO_PLUGIN_END
