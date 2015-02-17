3c0665fa08166c6e6b2b030993ab7b7c1b26a213
Robin Sommer <robin@icir.org>
2015-02-17 15:34:56 -0800

    Skeleton tweak.

--- plugin-support/skeleton/src/Plugin.cc
+++ plugin-support/skeleton/src/Plugin.cc
@@ -10,7 +10,7 @@ plugin::Configuration Plugin::Configure()
 	plugin::Configuration config;
 	config.name = "@PLUGIN_NAMESPACE@::@PLUGIN_NAME@";
 	config.description = "<Insert description>";
-	config.version.major = 1;
-	config.version.minor = 0;
+	config.version.major = 0;
+	config.version.minor = 1;
 	return config;
 	}
