7fe80115aa159119febc623cec0660774bcae50c
Jon Siwek <jsiwek@corelight.com>
2021-01-19 21:05:52 -0800

    Updating CHANGES and VERSION.

--- zeek-archiver.cc
+++ zeek-archiver.cc
@@ -23,7 +23,7 @@
 #include <vector>
 #include <set>
 
-constexpr auto ZEEK_ARCHIVER_VERSION = "v0.7.0-2";
+constexpr auto ZEEK_ARCHIVER_VERSION = "v0.7.0-4";
 
 struct Options {
 	std::string src_dir;
