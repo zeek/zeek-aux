b4fa813e2ce452432945dc040a82b60303353e65
Christian Kreibich <christian@corelight.com>
2024-03-29 19:15:54 -0700

    Remove zeek-archiver's CI setup, yesss

--- zeek-archiver/ci/freebsd/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Preparing FreeBSD environment"
-sysctl hw.model hw.machine hw.ncpu
-set -e
-set -x
-
-env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg install -y bash git cmake
--- zeek-archiver/ci/macos/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Preparing macOS environment"
-sysctl hw.model hw.machine hw.ncpu hw.physicalcpu hw.logicalcpu
-set -e
-set -x
-
-brew install cmake
