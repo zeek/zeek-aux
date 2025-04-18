b3c996cee66f765c31051689096ac0e3beda35a1
Tim Wojtulewicz <tim@corelight.com>
2025-04-18 13:02:02 -0700

    Add ruff to pre-commit using zeek's configuration

--- devel-tools/zeek_lldb_utils.py
+++ devel-tools/zeek_lldb_utils.py
@@ -1,5 +1,5 @@
 import lldb
-from ansi.color import fg, bg
+from ansi.color import fg
 from ansi.color.fx import reset
 from linereader import getline
 
@@ -8,9 +8,9 @@ script_lines = {}
 
 # TODO: make this check that the thread is actually stopped and return an error if not
 
-@lldb.command('btz')
-def backtrace_zeek(debugger, command, exe_ctx, result, d):
 
+@lldb.command("btz")
+def backtrace_zeek(debugger, command, exe_ctx, result, d):
     selected_thread = exe_ctx.GetProcess().GetSelectedThread()
     thread = exe_ctx.GetThread()
 
@@ -24,19 +24,23 @@ def backtrace_zeek(debugger, command, exe_ctx, result, d):
 
     for frame in thread.get_thread_frames():
         frame_output = f"  {'*' if frame.idx == selected_frame else ' '} "
-        frame_output += f'{frame}'
+        frame_output += f"{frame}"
 
-        this = frame.FindVariable('this')
+        this = frame.FindVariable("this")
         if this:
-            loc_ptr = this.GetChildMemberWithName('location')
-            if loc_ptr and loc_ptr.GetType().GetName() == 'zeek::detail::Location *':
+            loc_ptr = this.GetChildMemberWithName("location")
+            if loc_ptr and loc_ptr.GetType().GetName() == "zeek::detail::Location *":
                 if loc_ptr.GetValueAsUnsigned() != 0:
-                    loc = frame.EvaluateExpression('*(this->location)')
-                    fname = loc.GetChildMemberWithName('filename').GetSummary().strip('"')
-                    line_no = loc.GetChildMemberWithName('first_line').GetValueAsUnsigned()
-                    frame_output += f'\n        {fg.green}zeek script:{reset} {fname}'
+                    loc = frame.EvaluateExpression("*(this->location)")
+                    fname = (
+                        loc.GetChildMemberWithName("filename").GetSummary().strip('"')
+                    )
+                    line_no = loc.GetChildMemberWithName(
+                        "first_line"
+                    ).GetValueAsUnsigned()
+                    frame_output += f"\n        {fg.green}zeek script:{reset} {fname}"
 
-                    fileinfo = f'{fname}:{line_no}'
+                    fileinfo = f"{fname}:{line_no}"
                     if fileinfo in script_lines:
                         line = script_lines[fileinfo]
                     else:
@@ -45,8 +49,9 @@ def backtrace_zeek(debugger, command, exe_ctx, result, d):
                         script_lines[fileinfo] = line
 
                     if line:
-                        line_hdr = f'line {line_no}'
-                        frame_output += f'\n        {fg.green}{line_hdr : >11}:{reset} {line}'
-
+                        line_hdr = f"line {line_no}"
+                        frame_output += (
+                            f"\n        {fg.green}{line_hdr: >11}:{reset} {line}"
+                        )
 
         print(frame_output)
